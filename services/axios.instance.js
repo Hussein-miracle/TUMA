f3f591e4f6c836f519877541393fdff472cfc115
Abdullahi <miraacle64@gmail.com>
2023-01-26 20:26:55 +0100

    feat: getting ready for stagingt

--- services/axios.instance.js
+++ services/axios.instance.js
@@ -1,14 +1,9 @@
 import axios from "axios";
 
-// const url = 'https://vtsp35pseg.us-east-1.awsapprunner.com/api';
-
-const baseURL = 'https://app.dagdag.fun/api';
 
 
 const instance = axios.create({
-  
-  baseURL: baseURL,
-
+  baseURL: import.meta.env.VITE_APP_API_BASE_URL,
   headers: {
     Accept: "application/json, text/plain, */*",
     "Content-Type": "application/json, multipart/form-data",
