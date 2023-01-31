1244d32dd19abff37f230668f3395b70cb1fe10e
Abdullahi <miraacle64@gmail.com>
2023-01-31 14:55:18 +0100

    bug: still trying to figure out why sumsub fails with err preset

--- pages/transaction-detail/index.vue
+++ pages/transaction-detail/index.vue
@@ -175,7 +175,7 @@ const fetchTrustPaymentDetail = async () => {
   isLoading.value = !false;
   setTimeout(() => {
     isLoading.value = false;
-    showSuccess.value=true;
+    showSuccess.value=!true;
   },5300);
 };
 onMounted(() => {
